de8bc39befeb1113a7065dba942f2a4b013b5e00
Tykhon Byshkin <tykhonbyshkin07@gmail.com>
2024-10-18 22:22:48 -0400

    add some features

--- B3.py
+++ B3.py
@@ -1,33 +1,60 @@
 import random
+import time
 #Tykhon Byshkin and Mohammad Khan 
 def start_game():
 
     colours = ("Red","Yellow", "Green", "Blue")
     ranks = list(range(1,11))
-    deck = [(rank, colour) for rank in ranks for colour in colours]
+    actions=["skip turn","+2"]
+    deck = [(rank, colour) for rank in ranks for colour in colours]+[(action,colour)for colour in colours for action in actions]
     random.shuffle(deck)
     p1 = [deck.pop(0) for _ in range (7)]
     p2 = [deck.pop(0) for _ in range (7)]
-    central_card = deck.pop(0)
-    main_loop(p1, p2, deck, central_card, 0)
+    central_deck = [deck.pop(0)]
+    main_loop(p1, p2, deck, central_deck, 0)
 
-def main_loop(p1, p2, deck, central_card, whose_turn):
+def main_loop(p1, p2, deck, central_deck, whose_turn):
     while len(p1)>0 and len(p2)>0:
         print(f"Player {whose_turn + 1}'s turn, here is your hand {p1}")
-        print(f"Central card is: {central_card}")
-        ans= int(input("You have a choice. You can (0) draw or (1) play"))
+        print(f"Central card is: {central_deck[0]}")
+        validcards=[]
+        for card in p1:
+            if valid_play(central_deck[0],card):
+                validcards.append((card,p1.index(card)+1))
+        if validcards==[]:
+            print("you must draw")
+            time.sleep(1)
+            p1.append(deck.pop(0))
+            p1,p2 = p2,p1
+            whose_turn = (whose_turn + 1) % 2
+            continue
+
+        ans= int(input("You have a choice. You can (0) draw or (1)play. "))
+        
         if ans == 1:
-            player_choice = int(input("which card to play?"))-1
-            valid = valid_play(central_card, p1[player_choice])
-            if valid :
-                central_card = p1.pop(player_choice)
+            print(validcards)
+            player_choice = int(input("which card to play? "))-1
+            valid = valid_play(central_deck[0], p1[player_choice])
+            while not valid:
+                player_choice = int(input("which card to play? "))-1
+                valid = valid_play(central_deck[0], p1[player_choice])
+
+            if valid:
+                central_deck.insert(0, p1.pop(player_choice))
+                
         if ans == 0:
             draw_card = deck.pop(0)
             p1.append(draw_card)
+        if central_deck[0][0]=="+2":
+            p2.append(deck.pop(0))
+            p2.append(deck.pop(0))
+        if deck==[]:
+            deck=central_deck
+            random.shuffle(deck)
         p1,p2 = p2,p1
         whose_turn = (whose_turn + 1) % 2
-
+        
 def valid_play(card1, card2):
-    return card1[0] == card2[0] or card[1] == card[1]
+    return card1[0] == card2[0] or card1[1] == card2[1]
 start_game()
 
