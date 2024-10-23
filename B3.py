762874ff6989cb14d9f65f4ade66e8284cfa2a5e
Tykhon Byshkin <tykhon.byshkin@dawsoncollege.qc.ca>
2024-10-23 17:23:37 -0400

    add some stuff

--- B3.py
+++ B3.py
@@ -6,7 +6,7 @@ def start_game():
     colours = ("Red","Yellow", "Green", "Blue")
     ranks = list(range(1,11))
     actions=["skip turn","+2"]
-    deck = [(rank, colour) for rank in ranks for colour in colours]+[(action,colour)for colour in colours for action in actions]
+    deck = [(rank, colour) for rank in ranks for colour in colours]+[(action,colour)for colour in colours for action in actions]+[("change colour") for _ in range(2)]
     random.shuffle(deck)
     p1 = [deck.pop(0) for _ in range (7)]
     p2 = [deck.pop(0) for _ in range (7)]
@@ -29,45 +29,60 @@ def main_loop(p1, p2, deck, central_deck, whose_turn):
             whose_turn = (whose_turn + 1) % 2
             continue
 
-        ans= int(input("You have a choice. You can (0) draw or (1)play. "))
-        
-        if ans == 1:
-            print(validcards)
-            player_choice = int(input("which card to play? "))-1
-            valid = valid_play(central_deck[0], p1[player_choice])
-            while not valid:
+        if whose_turn ==0:
+
+            ans= int(input("You have a choice. You can (0) draw or (1)play. "))
+            if ans == 1:
+                print(validcards)
                 player_choice = int(input("which card to play? "))-1
                 valid = valid_play(central_deck[0], p1[player_choice])
+                while not valid:
+                    player_choice = int(input("which card to play? "))-1
+                    valid = valid_play(central_deck[0], p1[player_choice])
 
-            if valid:
-                central_deck.insert(0, p1.pop(player_choice))
+                if valid:
+                    central_deck.insert(0, p1.pop(player_choice))
+                    
+                # Check if player has only one card left
+                if len(p1) == 1:
+                    uno_call = input("You have only one card left! Say 'UNO': ")
+                    if uno_call != "UNO":  
+                        print("You didn't say 'UNO'! Drawing two penalty cards...")
+                        p1.append(deck.pop(0))
+                        p1.append(deck.pop(0))
+                    
+            if ans == 0:
+                draw_card = deck.pop(0)
+                p1.append(draw_card)
+            if central_deck[0][0]=="+2":
+                p2.append(deck.pop(0))
+                p2.append(deck.pop(0))
                 
-            # Check if player has only one card left
-            if len(p1) == 1:
-                uno_call = input("You have only one card left! Say 'UNO': ")
-                if uno_call != "UNO":  
-                    print("You didn't say 'UNO'! Drawing two penalty cards...")
-                    p1.append(deck.pop(0))
-                    p1.append(deck.pop(0))
-                
-        if ans == 0:
-            draw_card = deck.pop(0)
-            p1.append(draw_card)
-        if central_deck[0][0]=="+2":
-            p2.append(deck.pop(0))
-            p2.append(deck.pop(0))
-        
+        else:
+            print(validcards)
+            central_deck.insert(0,p1.pop(validcards[0][1]-1))
+            
         if deck==[]:
             deck=central_deck
             central_deck.clear()
             random.shuffle(deck)
             central_deck=[deck.pop(0)]
+        
+        if central_deck[0]=="change colour":
+            colour_choice=input("What colour do you want?: ")
+            while not colour_choice in ("Red","Yellow", "Green", "Blue"):
+               colour_choice=input("What colour do you want?: ")
+            central_deck[0]==(None,colour_choice)   
         p1,p2 = p2,p1
+        print(central_deck)
         if central_deck[0][0]=="skip turn":
             whose_turn+=1
         whose_turn = (whose_turn + 1) % 2
         
 def valid_play(card1, card2):
+    if card2=="change colour":
+        return True
+
     return card1[0] == card2[0] or card1[1] == card2[1]
 start_game()
 
